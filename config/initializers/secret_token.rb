e04cba1cb88dd589232f4c3159ee6a66ab6531dd
htada <tada.hideaki@gmail.com>
2013-04-08 23:21:27 +0900

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fatdownchart::Application.config.secret_token = '61a28bd8dc5e3242200fd16bb89086c278007797c884d8ffd4959bfa29553d384d7213d32bd57da4c20b53b6910b48230703ca3beb189e6cd449d84ede82857d'
